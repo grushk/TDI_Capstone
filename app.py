48d871cc37f7d67dbad6dfeeca0ac08a4d4b66b0
Grush Khalsa <grushk@gmail.com>
2017-01-27 17:51:53 -0800

    update

--- app.py
+++ app.py
@@ -21,23 +21,24 @@ def getitem(obj, item, default):
 
 data=pd.read_csv('static/data/small_sample.csv')
 
-
-@app.route("/")
+#    {{ js_resources|indent(4)|safe }}
+#    {{ css_resources|indent(4)|safe }}
+#@app.route("/")
 def histogram():
 	js_resources = INLINE.render_js()
 	css_resources = INLINE.render_css()
 
 	plot = figure(width=300, height=300)
 	plot.vbar(x=[1, 2, 3], width=0.5, bottom=0, top=[1,2,3], color="#CAB2D6")
-	script, div = components(p)
-	html = flask.render_template(
-		'index.html',
-		plot_script=script,
-		plot_div=div,
-		js_resources=js_resources,
-		css_resources=css_resources
-	)
-	return encode_utf8(html)
+	script, div = components(plot)
+	#html = flask.render_template(
+	#	'index.html',
+	#	plot_script=script,
+	#	plot_div=div
+	#	#js_resources=js_resources,
+	#	#css_resources=css_resources
+	#)
+	return script, div #encode_utf8(html)
 
 
 @app.route('/')
@@ -47,9 +48,12 @@ def main():
 
 @app.route('/index')
 def index():
-	return flask.render_template('index.html')
+	script, div = histogram()
+	#print(script, div)
+	print(div)
+	return flask.render_template('index.html',plot_script=script, plot_div=div)
 
 
 if __name__ == "__main__":
 	print(__doc__)
-	app.run(debug=False)
+	app.run(debug=True)
