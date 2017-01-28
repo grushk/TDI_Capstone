ce60241d23f27e929b4aa70f2ca5bafed7191337
Grush Khalsa <grushk@gmail.com>
2017-01-27 16:10:09 -0800

    update

--- app.py
+++ app.py
@@ -26,18 +26,17 @@ data=pd.read_csv('static/data/small_sample.csv')
 def histogram():
 	js_resources = INLINE.render_js()
 	css_resources = INLINE.render_css()
-	
+
 	plot = figure(width=300, height=300)
 	plot.vbar(x=[1, 2, 3], width=0.5, bottom=0, top=[1,2,3], color="#CAB2D6")
 	script, div = components(p)
-		html = flask.render_template(
-			'index.html',
-			plot_script=script,
-			plot_div=div,
-			js_resources=js_resources,
-			css_resources=css_resources,
-			ticker=stock
-		)
+	html = flask.render_template(
+		'index.html',
+		plot_script=script,
+		plot_div=div,
+		js_resources=js_resources,
+		css_resources=css_resources
+	)
 	return encode_utf8(html)
 
 
